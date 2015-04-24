76a74fa8c7b7b421582c798e563f7ebebb26280c
Mark Seidenstricker <mjseid@monsanto.com>
2015-04-24 15:19:56 -0500

    updated filters for alert module and using statsdecoder to interpert payload as double vs string

--- heka/jobhealth.lua
+++ heka/jobhealth.lua
@@ -3,7 +3,7 @@ local alert = require "alert"
 require "string"
 
 function process_message ()
-   local out_message = "DEA available memory ratio below threshold"
+   local out_message = "Something is not healthy"
    local ts = read_message("Timestamp")
    alert.set_throttle(60e9)
    alert.send(ts, out_message)
