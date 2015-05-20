8095514c0e1e7fad50e115aef3a8a27de1f55b16
Bhaskar Dutta <bdutta19@users.noreply.github.com>
2015-05-20 00:21:05 -0500

    Update mon_anomaly_w.lua

--- heka/mon_anomaly_w.lua
+++ heka/mon_anomaly_w.lua
@@ -6,34 +6,51 @@ algorithms
 
 --]]
 
+local alert = require "alert"
 require("string")
-
-function trnslt_anomaly_config(pl,mon_cfg,win,hwin )
-   local payl = pl or 'stats'
-   local mon_anomaly_config = mon_cfg or 'spikes:1 slowchange:1'
+function trnslt_anomaly_config(pl,mon_cfg)
+   local payl = pl or 'Stats'
+   local mon_anomaly_config = mon_cfg
 
    local t_anomcfg = {}
-   for k,v in string.gmatch(mon_anomaly_config,"(%w+):(%d+)")  do t_anomcfg[k] = v end
-   local a_fld = ''
+   for k,v in string.gmatch(mon_anomaly_config,"(%w+):(%d*%,?%d*%,?%d*)")  do t_anomcfg[k] = v end
+   local fld = ''
    local a_anomcfg = ' '
+   local win = 0
+   local hwin = 0
    local ky = ''
    local anon_str = ''
 
    for k in pairs(t_anomcfg)
      do
-       local a_fld = t_anomcfg[k] or ''
-       ky = string.lower(k) 
-    
-       if ky == 'spikes' then
-         anon_str = 'roc("' .. payl .. '",' .. a_fld .. ','.. win .. ',' .. hwin ..',2,false,true)'
+       if string.find(t_anomcfg[k],",") then
+          local i = 1
+          for k in string.gmatch(t_anomcfg[k],"([^"..",".."]+)") 
+             do 
+               if i==1 then
+                 fld = k
+               elseif i==2 then
+                 win = k
+               elseif i==3 then
+                  hwin = k
+               end    
+               i=i+1 
+            end
+       else   
+         fld = t_anom[k]
+       end  
+        
+       ky = string.lower(k)
+       if ky == 'spike' then
+         anon_str = 'roc("' .. payl .. '",' .. fld .. ','.. win .. ',' .. hwin ..',3,false,false)'
        elseif ky == 'slowchange' then
-         anon_str = 'mww("' .. payl .. '",' .. a_fld .. ','.. win .. ',' .. hwin ..',0.001,any)'
+         anon_str = 'mww("' .. payl .. '",' .. fld .. ','.. win .. ',' .. hwin ..',0.0002,any)'
        elseif ky == 'creepingchange' then
-         anon_str = 'mww_nonparameteric("'.. payl .. '",' .. a_fld .. ','.. win .. ',' .. hwin ..',0.60)'
+         anon_str = 'mww_nonparameteric("'.. payl .. '",' .. fld .. ','.. win .. ',' .. hwin ..',0.60)'
        elseif ky == 'breakout' then
          anon_str = ''
        elseif ky == 'breakdown' then
-         anon_str = 'roc("' .. payl .. '",' .. a_fld .. ',3,0,1,true,false)'
+         anon_str = 'roc("' .. payl .. '",' .. fld .. ',3,0,1,true,false)'
        elseif ky == 'threshold' then
          anon_str = ' '
        end
